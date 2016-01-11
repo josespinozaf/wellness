27e01f3a80a1231d6a32c95aa96c575f73b4f589
José Pablo Espinoza Farías <cote_786@hotmail.com>
2016-01-11 12:28:02 -0300

    calidad en fit

--- fitnessgram.php
+++ fitnessgram.php
@@ -5,48 +5,6 @@ require_once($CFG->dirroot . '/my/lib.php');
 
 redirect_if_major_upgrade_required();
 
-// **Conectando a la base de datos**
-include ("connect.php");
-
-//** USUARIO
-$userid= $USER->id;
-$usermail= $USER->email;
-// **Peticion al SQL**
-
-$result = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
-if (!$result) {
-	die("Error en la peticion SQL: " . mysql_error());
-}
-
-$result2 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
-if (!$result) {
-	die("Error en la peticion SQL: " . mysql_error());
-}
-
-$result3 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
-if (!$result) {
-	die("Error en la peticion SQL: " . mysql_error());
-}
-
-$result4 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
-if (!$result) {
-	die("Error en la peticion SQL: " . mysql_error());
-}
-	
-$result5 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
-	if (!$result) {
-		die("Error en la peticion SQL: " . mysql_error());
-	}
-	
-$result6 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
-		if (!$result) {
-			die("Error en la peticion SQL: " . mysql_error());
-}
-
-// **Mostrando los resultados
-//  while ($row = mysql_fetch_array($result)) {
- 	
-//  }
 
 $edit   = optional_param('edit', null, PARAM_BOOL);    // Turn editing on and off
 $reset  = optional_param('reset', null, PARAM_BOOL);
@@ -103,81 +61,9 @@ echo $OUTPUT->header ();
   <head>
 	<link rel="stylesheet" type="text/css" href="style.css" media="screen">
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-      google.load("visualization", "1.1", {packages:["table"]});
-      google.setOnLoadCallback(drawTable);
-      function drawTable() {
-          var data = new google.visualization.DataTable();
-          data.addColumn('string', 'Año');
-          data.addColumn('string', 'Talla');
-          data.addColumn('string', 'Peso');
-          data.addColumn('string', 'IMC');
-          data.addColumn('string', 'Suma MM');
-          data.addColumn('string', '% Grasa');
-          <?php while ($row = mysql_fetch_array($result)) { ?>
-          data.addRows([
-          ['<?php echo $row['Ano']?>','<?php echo $row['Talla']?>','<?php echo $row['Peso']?>','<?php echo $row['IMC']?>','<?php echo $row['Suma mm']?>','<?php echo $row['%Grasa']?>'],
-          ]);
-<?php }?>
-        var table = new google.visualization.Table(document.getElementById('table_div1'));
-
-        table.draw(data, {showRowNumber: false, width: '80%', height: '80%'});
-      }
-    </script>
-    
-  	<script type="text/javascript">
-      google.load("visualization", "1.1", {packages:["table"]});
-      google.setOnLoadCallback(drawTable);
-      function drawTable() {
-          var data = new google.visualization.DataTable();
-          data.addColumn('string', 'Año');
-          data.addColumn('string', 'Abd');
-          data.addColumn('string', 'Push Up');
-          <?php while ($row = mysql_fetch_array($result2)) { ?>
-          data.addRows([
-        ['<?php echo $row['Ano']?>','<?php echo $row['Abd']?>','<?php echo $row['Push Up']?>'],
-         ]);
-          <?php }?>
-        var table = new google.visualization.Table(document.getElementById('table_div2'));
-        table.draw(data, {showRowNumber: false, width: '70%', height: '70%'});
-      }
-    </script>
-    <script type="text/javascript">
-      google.load("visualization", "1.1", {packages:["table"]});
-      google.setOnLoadCallback(drawTable);
-      function drawTable() {
-          var data = new google.visualization.DataTable();
-          data.addColumn('string', 'Año');
-          data.addColumn('string', 'Sit&Reach-D');
-          data.addColumn('string', 'Sit&Reach-I');
-          data.addColumn('string', 'Trunk Lift');
-          <?php while ($row = mysql_fetch_array($result3)) { ?>
-          data.addRows([
-        ['<?php echo $row['Ano']?>','<?php echo $row['Sit&reach-D']?>','<?php echo $row['Sit&reach-IZ']?>','<?php echo $row['Trunk Lift']?>'],
-            ]);
-          <?php }?>
-        var table = new google.visualization.Table(document.getElementById('table_div3'));
-        table.draw(data, {showRowNumber: false, width: '70%', height: '70%'});
-      }
-    </script>
-  	<script type="text/javascript">
-      google.load("visualization", "1.1", {packages:["table"]});
-      google.setOnLoadCallback(drawTable);
-      function drawTable() {
-          var data = new google.visualization.DataTable();
-          data.addColumn('string', 'Año');
-          data.addColumn('string', 'Pacer');
-          data.addColumn('string', 'Vo2 Max');
-          <?php while ($row = mysql_fetch_array($result4)) { ?>
-          data.addRows([
-                        ['<?php echo $row['Ano']?>','<?php echo $row['Nivel']?>','<?php echo $row['Vo2 max']?>'],
-                            ]);
-                          <?php }?>
-                        var table = new google.visualization.Table(document.getElementById('table_div4'));
-                        table.draw(data, {showRowNumber: false, width: '70%', height: '70%'});
-                      }
-    </script> 
+   
   <?php
+  include ("functions.php");
 $con=mysql_connect("localhost","root","") or die("Failed to connect with database!!!!");
 mysql_select_db("moodle", $con); 
 // The Chart table contains two fields: weekly_task and percentage
--- functions.php
+++ functions.php
@@ -1,1 +1,128 @@
+<html>
+<head>
 <?php
+
+include ("connect.php");
+
+//** USUARIO
+$userid= $USER->id;
+$usermail= $USER->email;
+// **Peticion al SQL**
+
+$result = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
+if (!$result) {
+	die("Error en la peticion SQL: " . mysql_error());
+}
+
+$result2 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
+if (!$result) {
+	die("Error en la peticion SQL: " . mysql_error());
+}
+
+$result3 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
+if (!$result) {
+	die("Error en la peticion SQL: " . mysql_error());
+}
+
+$result4 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
+if (!$result) {
+	die("Error en la peticion SQL: " . mysql_error());
+}
+	
+$result5 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
+	if (!$result) {
+		die("Error en la peticion SQL: " . mysql_error());
+	}
+	
+$result6 = mysql_query("SELECT * FROM fitnessgram WHERE email='$usermail'", $db);
+		if (!$result) {
+		die("Error en la peticion SQL: " . mysql_error());
+}?>
+<script type="text/javascript" >
+	google.load("visualization", "1.1", {packages:["table"]});
+	google.setOnLoadCallback(drawTable);
+	function drawTable() {
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'Año');
+		data.addColumn('string', 'Talla');
+		data.addColumn('string', 'Peso');
+		data.addColumn('string', 'IMC');
+		data.addColumn('string', 'Suma MM');
+		data.addColumn('string', '% Grasa');
+	          <?php while ($row = mysql_fetch_array($result)) { ?>
+	          data.addRows([
+	          ['<?php echo $row['Ano']?>','<?php echo $row['Talla']?>','<?php echo $row['Peso']?>','<?php echo $row['IMC']?>','<?php echo $row['Suma mm']?>','<?php echo $row['%Grasa']?>'],
+	          ]);
+	<?php }?>
+	        var table = new google.visualization.Table(document.getElementById('table_div1'));
+	
+	        table.draw(data, {showRowNumber: false, width: '80%', height: '80%'});
+	      }
+</script>
+	<script type="text/javascript" >
+
+      google.load("visualization", "1.1", {packages:["table"]});
+      google.setOnLoadCallback(drawTable);
+      function drawTable() {
+          var data = new google.visualization.DataTable();
+          data.addColumn('string', 'Año');
+          data.addColumn('string', 'Abd');
+          data.addColumn('string', 'Push Up');
+          <?php while ($row = mysql_fetch_array($result2)) { ?>
+          data.addRows([
+        ['<?php echo $row['Ano']?>','<?php echo $row['Abd']?>','<?php echo $row['Push Up']?>'],
+         ]);
+          <?php }?>
+        var table = new google.visualization.Table(document.getElementById('table_div2'));
+        table.draw(data, {showRowNumber: false, width: '70%', height: '70%'});
+      }
+
+    </script>
+    
+    <script type="text/javascript" >
+
+      google.load("visualization", "1.1", {packages:["table"]});
+      google.setOnLoadCallback(drawTable);
+      function drawTable() {
+          var data = new google.visualization.DataTable();
+          data.addColumn('string', 'Año');
+          data.addColumn('string', 'Sit&Reach-D');
+          data.addColumn('string', 'Sit&Reach-I');
+          data.addColumn('string', 'Trunk Lift');
+          <?php while ($row = mysql_fetch_array($result3)) { ?>
+          data.addRows([
+        ['<?php echo $row['Ano']?>','<?php echo $row['Sit&reach-D']?>','<?php echo $row['Sit&reach-IZ']?>','<?php echo $row['Trunk Lift']?>'],
+            ]);		
+          <?php }?>
+        var table = new google.visualization.Table(document.getElementById('table_div3'));
+        table.draw(data, {showRowNumber: false, width: '70%', height: '70%'});
+      }
+    
+    </script>
+    <script type="text/javascript" >
+      google.load("visualization", "1.1", {packages:["table"]});
+      google.setOnLoadCallback(drawTable);
+      function drawTable() {
+          var data = new google.visualization.DataTable();
+          data.addColumn('string', 'Año');
+          data.addColumn('string', 'Pacer');
+          	data.addColumn('string', 'Vo2 Max');
+          <?php while ($row = mysql_fetch_array($result4)) { ?>
+          data.addRows([
+                        ['<?php echo $row['Ano']?>','<?php echo $row['Nivel']?>','<?php echo $row['Vo2 max']?>'],
+                            ]);
+                          <?php }?>
+                        var table = new google.visualization.Table(document.getElementById('table_div4'));
+                        table.draw(data, {showRowNumber: false, width: '70%', height: '70%'});
+                      }   
+      </script> 
+    
+	
+	
+	
+	
+	
+
+</head>
+
+</html>
