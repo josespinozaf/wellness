902a5e3aff3ec466e0154f47e5dfd6e92ed46d89
sfloresch <sebaflores@alumnos.uai.cl>
2016-01-11 11:48:43 -0300

    Agregando Lang
    Es y En, se agregan algunos string.

--- lang/en/local_wellness.php
+++ lang/en/local_wellness.php
@@ -1,4 +1,69 @@
 <?php
+//**Pagina General**
+$string['titulopagina'] = 'DeportesUAI';
 
-$string['titleasistencias']       = 'Asistencias';
+//**Barra Navegacion**
+
+$string['navpagprinc'] = 'Home Page';
+$string['navrutinas'] = 'Routines';
+$string['navfitnessgram'] = 'Fitnessgram';
+$string['navasistencias'] = 'Assists';
+$string['naveventos'] = 'Events';
+
+//**Mis Rutinas**
+
+//**Fitnessgram**
+
+$string['antropometria'] = 'Anthropometry';
+$string['fuerza'] = 'Strength';
+$string['flexibilidad'] = 'Flexibility';
+$string['resistencia'] = 'Resistance';
+$string['grafico'] = 'Graph';
+$string['ano'] = 'Year';
+$string['talla'] = 'Height';
+$string['peso'] = 'Weight';
+$string['imc'] = 'IMC';
+$string['grasa'] = 'Fat Percentage';
+$string['abd'] = 'Abdominals';
+$string['pushup'] = 'Push ups';
+$string['sitandreachd'] = 'Right Sit and Reach';
+$string['sitandreachi'] = 'Left Sit and Reach';
+$string['trunklift'] = 'Trunk Lift';
+$string['pacer'] = 'Field Runs';
+$string['vo2max'] = 'Aerobic Capacity';
+
+//**Mis Asistencias**
+
+$string['cantidadasistencias'] = 'The assists quantity in this period is: ';
+$string['tabasistencias'] = 'Assists Registry';
+$string['tabgraph1'] = 'Completition Percentage';
+$string['tabgraph2'] = 'Actual-Ideal Comparation';
+$string['titleasistencias'] = 'Assists';
+$string['titulografico1'] = 'My Assists';
+$string['titulografico2'] = 'Actual vs ideal assists';
+$string['ejex'] = 'Months';
+$string['ejey'] = 'Assists';
+$string['periodo'] = 'Period';
+$string['fecha'] = 'Date';
+$string['horainicio'] = 'Start Time';
+$string['horatermino'] = 'End Time';
+$string['actividad'] = 'Activity';
+$string['asistencia'] = 'Assist';
+
+//**Eventos**
+
+
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
 ?>
--- lang/es/local_wellness.php
+++ lang/es/local_wellness.php
@@ -1,13 +1,73 @@
 <?php
-$string['titleasistencias'] = 'Asistencias';
-$string['titleestadisticas'] = 'Mis Estadísticas';
-$string['titulografico1'] = 'Mis Asistencias';
-$string['titulografico2'] = 'Asistencias actual- ideal';
-$string['ejex'] = 'Meses';
-$string['ejey'] = 'Asistencia';
-$string['antropometria'] = 'Antopometría';
+//**Pagina General**
+$string['titulopagina'] = 'DeportesUAI';
+
+//**Barra Navegacion**
+
+$string['navpagprinc'] = 'Página Principal';
+$string['navrutinas'] = 'Rutinas';
+$string['navfitnessgram'] = 'Fitnessgram';
+$string['navasistencias'] = 'Asistencias';
+$string['naveventos'] = 'Eventos';
+
+//**Mis Rutinas**
+
+//**Fitnessgram**
+
+$string['antropometria'] = 'Antropometría';
 $string['fuerza'] = 'Fuerza';
 $string['flexibilidad'] = 'Flexibilidad';
 $string['resistencia'] = 'Resistencia';
+$string['grafico'] = 'Gráfico';
+$string['ano'] = 'Año';
+$string['talla'] = 'Altura';
+$string['peso'] = 'Peso';
+$string['imc'] = 'IMC';
+$string['grasa'] = 'Porcentaje Grasa';
+$string['abd'] = 'Abdominales';
+$string['pushup'] = 'Flexiones de Brazo';
+$string['sitandreachd'] = 'Alcance Derecho';
+$string['sitandreachi'] = 'Alcance Izquierdo';
+$string['trunklift'] = 'Levantamiento del Tronco';
+$string['pacer'] = 'Largos de cancha';
+$string['vo2max'] = 'Capacidad Aeróbica';
+
+//**Mis Asistencias**
+
+$string['cantidadasistencias'] = 'La cantidad de asistencias en este período son: ';
+$string['tabasistencias'] = 'Registro Asistencias';
+$string['tabgraph1'] = 'Porcentaje completadas';
+$string['tabgraph2'] = 'Comparación Actual-Ideal';
+$string['titleasistencias'] = 'Asistencias';
+$string['titulograficoas1'] = 'Mis Asistencias';
+$string['titulograficoas2'] = 'Asistencias actual- ideal';
+$string['ejex'] = 'Meses';
+$string['ejey'] = 'Asistencia';
+$string['periodo'] = 'Período';
+$string['fecha'] = 'Fecha';
+$string['horainicio'] = 'Hora Inicio';
+$string['horatermino'] = 'Hora Termino';
+$string['actividad'] = 'Actividad';
+$string['asistencia'] = 'Asistencia';
+
+//**Eventos**
+
+
+
+
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+$string[''] = '';
+
 
 ?>
