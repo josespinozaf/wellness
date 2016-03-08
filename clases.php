92789175c112759f392d26f353ab9fe43e4beb84
José Pablo Espinoza Farías <cote_786@hotmail.com>
2016-03-08 18:32:33 -0300

    Creación de clases

--- clases.php
+++ clases.php
@@ -4,47 +4,41 @@ require_once(dirname(__FILE__) . '/../../config.php');
 require_once($CFG->dirroot . '/my/lib.php');
 include ("connect.php");
 
-redirect_if_major_upgrade_required();
+$userid= $USER->id;
+$usermail= $USER->email;
 
-require_login();
-
-$edit   = optional_param('edit', null, PARAM_BOOL);    // Turn editing on and off
-$reset  = optional_param('reset', null, PARAM_BOOL);
-
-$params = array();
-$PAGE->set_context($context);
-$PAGE->set_url('/local/wellness/clases.php', $params);
-$PAGE->set_pagelayout('standard');
-$PAGE->set_pagetype('local-clases-index');
-$PAGE->blocks->add_region('content');
-$PAGE->set_subpage($currentpage->id);
-$PAGE->set_title(get_string('titleclases','local_wellness'));
-$PAGE->set_heading($header);
-$PAGE->navbar->add(get_string('navclases','local_wellness'), new moodle_url('/local/wellness/clases.php'));
-
-
-echo $OUTPUT->header ();
+echo $OUTPUT->header();
 
 $result = mysql_query("SELECT DISTINCT mp.* , mc.* FROM mdl_course_modules as mc
-		INNER JOIN mdl_page as mp ON mc.course = mp.course AND mc.instance = mp.id WHERE mp.course = 1 and mc.module = 15 GROUP BY mp.name", $db);
+		INNER JOIN mdl_page as mp ON mc.course = mp.course AND mc.instance = mp.id
+		WHERE mp.course = 1 and mc.module = 15
+		GROUP BY mp.name", $db);
 
 if (!$result) {
 	die("Error en la peticion SQL: " . mysql_error());
 }
-
-$resultfoto = mysql_query("SELECT DISTINCT mp.* , pp.* FROM mdl_page as mp
-		INNER JOIN page_pix as pp ON mp.id = pp.pageid", $db);
-
-while ($row = mysql_fetch_array($result)) {
-
-	
-	echo "<a href='/../../moodle/mod/page/view.php?id=".$row['id']."'><img src='http://www.clubcamposevilla.com/news_images/section/201209191359370.Clasesdeportivas.jpg' border = '0' alt=".$row['name']." width='200' height='200'></img></a>";
-	echo "  ";
+$resultfoto = mysql_query("SELECT DISTINCT mp.* , pp.* FROM mdl_page as mp INNER JOIN page_pix as pp ON mp.id = pp.pageid", $db);
+$clases = array();
+$fotos = array();
+while ($clase =  mysql_fetch_assoc($result))
+{
+	$clases[] = $clase;
 }
-
-
-//echo '<img src="http://espaciorosa.cl/construccion.jpg"></img>';
-
-
+while ($foto =  mysql_fetch_assoc($resultfoto))
+{
+	$fotos[] = $foto;
+}
+foreach ($clases as $clase)
+{
+	foreach ($fotos as $foto)
+	{
+	if 	($clase['name'] == $foto['name']){		
+	echo "<a href='/../../moodle/mod/page/view.php?id=".$clase['id']."'><img src=".$foto['pix']." border = '0' alt=".$clase['name']." width='200' height='200'></img></a><br>";
+	}
+	}
+}
+		?>
+		
+		<?php 
 echo $OUTPUT->footer();
-?>
+	?>
