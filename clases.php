4a1b220df0c2968562054614cafce16327a50cd2
sfloresch <sebaflores@alumnos.uai.cl>
2016-03-22 16:33:35 -0300

    Uploading shits
    los uploads a base de datos de fotos

--- clases.php
+++ clases.php
@@ -43,6 +43,18 @@ foreach ($clases as $clase)
 }
 		?>
 		
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
 		<?php 
 echo $OUTPUT->footer();
 	?>
