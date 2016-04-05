dbe9962d55c4dc0dc3bc74b59442fc8cdf214eca
sfloresch <sebaflores@alumnos.uai.cl>
2016-04-05 15:29:17 -0300

    cambio prueba query
    para books

--- pruebaquery.php
+++ pruebaquery.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="style.css" media="screen">
 <?php
 global $USER, $CFG;
 require_once(dirname(__FILE__) . '/../../config.php');
@@ -10,91 +11,41 @@ $usermail= $USER->email;
 echo $OUTPUT->header();
 
 $result = mysql_query("SELECT DISTINCT mp.* , mc.* FROM mdl_course_modules as mc
-		INNER JOIN mdl_page as mp ON mc.course = mp.course AND mc.instance = mp.id 
-		WHERE mp.course = 1 and mc.module = 15 
+		INNER JOIN mdl_book as mp ON mc.course = mp.course AND mc.instance = mp.id
+		WHERE mp.course = 1 and mc.module = 3
 		GROUP BY mp.name", $db);
 
 if (!$result) {
 	die("Error en la peticion SQL: " . mysql_error());
 }
-
-$resultfoto = mysql_query("SELECT DISTINCT mp.* , pp.* FROM mdl_page as mp
-							INNER JOIN page_pix as pp ON mp.id = pp.pageid", $db);
-
-if (!$resultfoto) {
-	die("Error en la peticion SQL: " . mysql_error());
-}
-
-
+//$resultfoto = mysql_query("SELECT DISTINCT mp.* , pp.* FROM mdl_page as mp INNER JOIN imagenes as pp ON mp.name = pp.nombre", $db);
 $clases = array();
-$fotos = array();
+//$fotos = array();
 while ($clase =  mysql_fetch_assoc($result))
 {
 	$clases[] = $clase;
 }
-while ($foto =  mysql_fetch_assoc($resultfoto))
-{
-	$fotos[] = $foto;
-}
+//while ($foto =  mysql_fetch_assoc($resultfoto))
+//{
+//	$fotos[] = $foto;
+//}
 foreach ($clases as $clase)
 {
-	foreach ($fotos as $foto)
-	{
- 	if 	($foto['name'] == $clase['name']){		
-// 	echo $clase['id'].'<br>';
-// 	echo $clase['name'].'<br>';
-// 	echo $foto['pix'].'<br>';
-// 	echo $clase['intro'].'<br>';
-
-	echo "<a href='/../../moodle/mod/page/view.php?id=".$clase['id']."'><img src=".$foto['pix']." border = '0' alt=".$clase['name']." width='200' height='200'>.    .</img></a>";
-	}
-	}
+	//foreach ($fotos as $foto)
+	//{
+	//if 	($clase['name'] == $foto['name']){	
+		echo '<div class="img">';	
+		echo "<a href='/../../moodle/mod/page/view.php?id=".$clase['id']."'>";
+		echo "<img  src='/../../moodle/local/wellness/imagen.php?nombre=".$clase['name']."' alt=".$clase['name']."></img></a>";
+		echo '<div class="desc">'.$clase['name'].'</div></div>';
+	//}
+	//}
 }
-
-
-	
-
-//  foreach($result as $listacursos){ 
-// 	$id= $listacursos-> id; 
-//  	$nombre= $listacursos-> name; 
-// 	echo $id; 
-// 	echo $nombre; 
-	
-//  } 
-
-
-
-// $result1 = mysql_query("SELECT DISTINCT mp.* , mc.*,pp.* FROM mdl_course_modules as mc
-// 		INNER JOIN mdl_page as mp ON mc.course = mp.course AND mc.instance = mp.id 
-// 		INNER JOIN page_pix as pp ON mp.name = pp.name
-// 		WHERE mp.course = 1 and mc.module = 15 GROUP BY mp.name", $db);
-
-// if (!$result1) {
-// 	die("Error en la peticion SQL: " . mysql_error());
-// }
-
-// $result3 = mysql_query("SELECT DISTINCT mp.* , mc.*, pp.* FROM mdl_page as mp
-// 		INNER JOIN page_pix as pp ON mp.id = pp.pageid AND mp.id = pp.id
-// 		INNER JOIN mdl_course_modules as mc ON mp.course = mc.course AND mc.instance = mp.id
-// 		WHERE mp.course = 1 and mc.module = 15 GROUP BY mp.name
-// 		", $db);
-
-// while ($row = mysql_fetch_array($result) && $row1 = mysql_fetch_array($resultfoto)) {
-// //   	 while (){
-// 	 	if ($row['instance'] == $row1['pageid'] ){
-// 	echo 'Name: '.$row['name'];
-// 	echo "<br>";
-// 	echo 'Foto: '.$row1['pix']; 	
-// 	echo "<br>";
-// 	echo 'course: '.$row['module'];
-// 	echo "<br>";
-// 	echo 'id: '.$row['id'];
-// 	echo "<br>";
-// 	//echo "<a href='/../../moodle/mod/page/view.php?id=".$row['id']."'>".$row['name']."</a>";
-// 	echo "<a href='/../../moodle/mod/page/view.php?id=".$row['id']."'><img src=".$row1['pix']." border = '0' alt=".$row['name']." width='200' height='200'></img></a><br>";
-// 	 	}
-//  }
-// //   	}	
-
+ echo "<form action='/../../moodle/local/wellness/formulariofoto.php' method='POST'>";
+ echo "<input type='submit' name='Agregar' value='Agregar foto a clase'>";
+ echo "<input type='submit' name='Cambiar' value='Cambiar foto a clase'>";
+ echo "</form>";
 echo $OUTPUT->footer();
 	?>
+	
+	
