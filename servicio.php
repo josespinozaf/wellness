bfe35d2602a4e54e22837f29886a929fc4089d3a
José Espinoza <josespinoza@alumnos.uai.cl>
2016-07-30 15:25:04 -0400

    java

--- servicio.php
+++ servicio.php
@@ -1,29 +1,28 @@
 <html>
 <body>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-Usuario:<input name="name" type="text" />
-Password:<input name="password" type="text" />
+Usuario:<input name="name" type="text" id="name"/>
+Password:<input name="password" type="text" id="password" />
 <input type='submit' name='enviar'>
 </form>
 <?php 
-if (isset($_POST['enviar'])){
-	
-$name= $_REQUEST['name'];
-$password= $_REQUEST['password'];
+if (isset($_POST['enviar'])){?>
+<script type="text/javascript">
+var name = document.getElementById("name").value;
+var password = document.getElementById("passwori").value
+var token = "KknDlkLKmmm909b998vb92368nJuijs6s544sd";
+var request = new XMLHttpRequest();
+request.open('POST', 'http://private-d36e3-uai1.apiary-mock.com/login{?token&?email&?password}');
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
+};
 
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "http://private-d36e3-uai1.apiary-mock.com/login{?token&?email&?password}");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-curl_setopt($ch, CURLOPT_POST, TRUE);
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-var_dump($response);
-}
-?>
+request.send();
+</script>
+<?php }?>
 </body>
 </html>
