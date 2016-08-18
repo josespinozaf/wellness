d58e4f5990fd148394d37df15f8961cf45b01682
José Espinoza <josespinoza@alumnos.uai.cl>
2016-08-18 18:43:47 -0300

    cambioss

--- servicio.php
+++ servicio.php
@@ -6,23 +6,23 @@ Password:<input name="password" type="text" id="password" />
 <input type='submit' name='enviar'>
 </form>
 <?php 
-if (isset($_POST['enviar'])){?>
-<script type="text/javascript">
-var name = document.getElementById("name").value;
-var password = document.getElementById("passwori").value
-var token = "KknDlkLKmmm909b998vb92368nJuijs6s544sd";
-var request = new XMLHttpRequest();
-request.open('POST', 'http://private-d36e3-uai1.apiary-mock.com/login{?token&?email&?password}');
-request.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
-  }
-};
+if (isset($_POST['enviar'])){
+$ch = curl_init();
 
-request.send();
-</script>
-<?php }?>
+curl_setopt($ch, CURLOPT_URL, "http://private-d36e3-uai1.apiary-mock.com/login?token=KknDlkLKmmm909b998vb92368nJuijs6s544sd&email=josespinoza%40alumnos.uai.cl&password=1234.AaA");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_POST, TRUE);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  "Content-Type: application/x-www-form-urlencoded"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
+}?>
 </body>
 </html>
