1287f1897a0f303c6eb24dd5f2d51435a5e11369
sfloresch <sebaflores@alumnos.uai.cl>
2016-09-01 15:28:28 -0300

    Servicios actualizados

--- servicio.php
+++ servicio.php
@@ -1,9 +1,13 @@
 <html>
 <body>
 <form action="servicio1.php" method="post">
-Usuario:<input name="usuario" type="text" id="usuario"/>
+Usuario:<input name="email" type="text" id="email"/>
 Password:<input name="password" type="text" id="password" />
 <input type='submit' name='enviar'>
 </form>
 </body>
 </html>
+<?php 
+session_start();
+session_unset();
+?>
--- servicio1.php
+++ servicio1.php
@@ -1,19 +1,21 @@
 <?php 
+session_start();
 if ($_POST) {
-                $cadena = 'KknDlkLKmmm909b998vb92368nJuijs6s544sd';
-                $usuario = $_POST['usuario'];
+                $token = 'KknDlkLKmmm909b998vb92368nJuijs6s544sd';
+                $email = $_POST['email'];
                 $password = $_POST['password'];
                
                 $url = 'http://webapitest.uai.cl/wellness/login';
+                //$url = 'http://private-74270a-uai2.apiary-mock.com/login';
                                               
-                echo "<br />Esta es la cadena: " . $cadena;
+                echo "<br />Esta es la cadena: " . $token;
                 echo "<br />Esta es la url: " . $url;
                
-                echo "<br />param1: " . $usuario;
-                echo "<br />param2: " . $password;
+                echo "<br />Email ingresado: " . $email;
+                echo "<br />Password ingresada: " . $password;
                 $curl_post_data = array(
-            									"token" => $cadena,
-                                               "email" => $usuario,
+            									"token" => $token,
+                                               "email" => $email,
                                                "password" => $password,
             );
                 $data_string = json_encode($curl_post_data);      
@@ -30,17 +32,17 @@ if ($_POST) {
                 ); 
  
                 echo '<br />';
-                echo $curl;
                 $curl_response = curl_exec($curl);
                
                 curl_close($curl);
                 echo '<br />';
-                echo $curl_response;
+                echo "El curl_response es (string complet):".$curl_response;
                               
                 $decoded = json_decode($curl_response);
-                echo "<br /><br />Token devuelto por el servicio: " . $decoded->token;
-               
-                $decoded = json_decode($curl_response);
+                $tokenApp = $decoded->tokenApp;
+                echo "<br /><br />TokenApp devuelto por el servicio: " . $decoded->tokenApp;
+                echo "<a href='http://localhost:81/moodle/local/wellness/serviciochino.php'>Link</a>";
+                $_SESSION["tokenApp"] = $tokenApp;
                
 }
 ?>
