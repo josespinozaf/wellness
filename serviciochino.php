a4bcd421627368ef767612edab2484ab69730df5
sfloresch <sebaflores@alumnos.uai.cl>
2016-09-01 15:32:42 -0300

    serv

--- serviciochino.php
+++ serviciochino.php
@@ -1,26 +1,51 @@
 <html>
 <body>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-Usuario:<input name="name" type="text" id="name"/>
+Usuario:<input name="email" type="text" id="email"/>
 Password:<input name="password" type="text" id="password" />
 <input type='submit' name='enviar'>
 </form>
 <?php 
-if (isset($_POST['enviar'])){?>
-<?php
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "http://private-905d21-duai.apiary-mock.com/login");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-curl_setopt($ch, CURLOPT_POST, TRUE);
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-var_dump($response);
-
-}?>
+session_start();
+if ($_POST) {
+                $token = 'KknDlkLKmmm909b998vb92368nJuijs6s544sd';
+                $tokenApp = $_SESSION["tokenApp"];
+               
+                //$url = 'http://webapitest.uai.cl/wellness/login';
+                $url = 'http://private-74270a-uai2.apiary-mock.com/InformacionPersonal';
+                                              
+                echo "<br />Esta es la cadena: " . $token;
+                echo "<br />Esta es la url: " . $url;
+               
+                echo "<br />TokenApp ingresado: " . $tokenApp;
+                $curl_post_data = array(
+            									"token" => $token,
+                                               "tokenApp" => $tokenApp                                               
+            );
+                $data_string = json_encode($curl_post_data);      
+               
+                $curl = curl_init();
+               
+                curl_setopt($curl, CURLOPT_POST, 1);
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                         
+    'Content-Type: application/json',                                                                               
+    'Content-Length: ' . strlen($data_string))                                                                      
+                ); 
+ 
+                echo '<br />';
+                $curl_response = curl_exec($curl);
+               
+                curl_close($curl);
+                echo '<br />';
+                echo "El curl_response es (string completo):".$curl_response;
+                              
+                $decoded = json_decode($curl_response);
+                echo "<br /><br />Nombre Completo devuelto por el servicio: " . $decoded->NombreCompleto;
+               
+}
+?>
 </body>
 </html>
