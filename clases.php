ca6bf3017f17c0668696e1eb69b6b1d5a2ba0561
sfloresch <sebaflores@alumnos.uai.cl>
2017-01-23 15:50:59 -0300

    Clases con objeto
    Query nueva, obsoleta borrada

--- clases.php
+++ clases.php
@@ -23,46 +23,16 @@ if (has_capability ( "local/wellness:seebutton", $context )) {
 	echo "</form>";
 }
 
-
 //Query para las clases
-$result= $DB->get_record_sql("SELECT DISTINCT mcm.* , mc.* FROM mdl_course_modules as mcm
-		INNER JOIN mdl_course as mc ON mcm.course = mc.id
-		WHERE mcm.module = 9
-		GROUP BY mc.fullname");
+$result = $DB->get_recordset_sql("SELECT DISTINCT mc.* , pp.* FROM mdl_course as mc INNER JOIN imagenes as pp ON mc.fullname = pp.nombre");
 
-$resultfoto = $DB->get_record_sql("SELECT DISTINCT mc.* , pp.* FROM mdl_course as mc INNER JOIN imagenes as pp ON mc.fullname = pp.nombre");
-
-//Mostrar clases
-//$clases = array ();
-//$fotos = array ();
-// while ( $clase = mysql_fetch_assoc ( $result ) ) {
-// 	$clases [] = $clase;
-// }
-// while ( $foto = mysql_fetch_assoc ( $resultfoto ) ) {
-// 	$fotos [] = $foto;
-// }
-
-$table = new html_table();
-$table->head = array('Course','Fullname');
-foreach ($result as $records) {
-	$course= $records->course;
-	$fullname = $records->fullname;
-	$link= "www.google.com";
-	$table->data[] = array($course, $fullname,'<a href="'.$link.'">View</a>');
-}
-
-echo html_writer::table($table);
-
-// foreach ( $result as $clase ) {
-// 	foreach ( $resultfoto as $foto ) {
-// 		if ($clase ['fullname'] == $foto ['nombre']) {
-// 			echo '<div class="img">';
-// 			echo "<a href='/../../moodle/course/view.php?id=" . $clase ['course'] . "'>";
-// 			echo "<img  src='/../../moodle/local/wellness/imagen.php?nombre=" . $clase ['fullname'] . "' alt=" . $clase ['fullname'] . "></img></a>";
-// 			echo '<div class="desc">' . $clase ['fullname'] . '</div></div>';
-// 		}
-// 	}
-// }
+foreach ($result as $rs){
+			echo '<div class="img">';
+			echo "<a href='/../../moodle/course/view.php?id=".$rs->id."'>";
+			echo "<img  src='/../../moodle/local/wellness/imagen.php?nombre=".$rs->fullname."' alt=". $rs->fullname."></img></a>";
+			echo '<div class="desc">'.$rs->fullname.'</div></div>';
+		}
+$result->close();
 
 //Footer
 echo $OUTPUT->footer ();
