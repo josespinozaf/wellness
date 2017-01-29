62df6f960e3e54a926be24ed0beca5b93bcbfadc
José Espinoza <josespinoza@alumnos.uai.cl>
2017-01-29 15:24:20 -0300

    Delete connect

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//Conexion a la base de datos
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "moodle";
-$db = mysql_connect($dbhost,$dbuser,$dbpass) or die('No se puede conectar a la base de datos.');
-mysql_select_db($dbname) or die('Problema al seleccionar la base de datos.');
-?>
